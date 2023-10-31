e3b8cc1a015db3a33128f29b27327b9288bb7b94
Eder venancio <ederpvenancio@gmail.com>
2023-10-31 16:42:26 -0300

    add big o notation

--- src/BigO.java
+++ src/BigO.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class BigO {
+public class BigO {
+
+        public static void main(String[] args) {
+            //  O(1) System.out.println(multiplyNumbers(2));
+
+             // 0(N) printNumbers(10);
+
+             // O(n²)printNumbersSquared(3);
+             // O(n³)printNumbersThree(3);
+
+             // O(n²+n) printNonDominant(3);
+
+            // System.out.println(sumNumbers(2));
+
+        }
+
 }

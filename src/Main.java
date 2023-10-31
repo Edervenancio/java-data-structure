17471c61437cfdf1e783c2c4d16e844beb0eb88a
Eder venancio <ederpvenancio@gmail.com>
2023-10-31 16:41:53 -0300

    add big o execution

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import static BigO.BigOConstant.multiplyNumbers;
+import static BigO.BigOLinear.printNumbers;
+import static BigO.BigOSquared.printNumbersSquared;
+import static BigO.NonDominantSquared.printNonDominant;
+import static BigO.SpaceComplexity.sumNumbers;
+
 public class Main {
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // O(1) System.out.println(multiplyNumbers(2));
+
+        // 0(N) printNumbers(10);
+
+        // O(n²)printNumbersSquared(3);
+        // O(n³)printNumbersThree(3);
+
+        // O(n²+n) printNonDominant(3);
+
+        System.out.println(sumNumbers(2));
+
     }
 }
